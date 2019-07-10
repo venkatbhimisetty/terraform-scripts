d4024f61b79a1487422a2357819fb21abd611149
venkatbhimisetty <bhimisettyvenkatasubbarao@gmail.com>
2019-07-10 09:51:19 +0000

    demo-2 added

---
+++ demo-2/vars.tf
@@ -0,0 +1,23 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-0cfee17793b08a293"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
